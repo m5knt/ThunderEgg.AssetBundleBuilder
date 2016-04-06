0ab7ddf015bfd2acc85cd10aa5a7b4f2214fb93f
m5knt <m5knt@inetsbb.com>
2016-04-07 03:21:46 +0900

    add launcher

---
+++ Unity3D.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d %~dp0
+for /f "usebackq" %%t in (`cd`) do set "Project=%%t"
+set "Unity3D=%ProgramFiles%/Unity"
+echo Unity3D : %Unity3D%
+echo Project : %Project%
+start "" "%Unity3D%/Editor/Unity" -projectPath "%Project%"
+ping -n 1 -w 5000 1.1.1.1 >nul
