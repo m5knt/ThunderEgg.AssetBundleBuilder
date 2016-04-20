db58e5c05674aa370fe01d6f85c28d0a60bb5fb0
m5knt <m5knt@inetsbb.com>
2016-04-21 02:16:46 +0900

    update

--- Unity3D.bat
+++ Unity3D.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d %~dp0
 for /f "usebackq" %%t in (`cd`) do set "Project=%%t"
-set "Unity3D=%ProgramFiles%/Unity"
+set "Unity3D=%ProgramFiles%/Unity 5.3.4p2"
 echo Unity3D : %Unity3D%
 echo Project : %Project%
 start "" "%Unity3D%/Editor/Unity" -projectPath "%Project%"
